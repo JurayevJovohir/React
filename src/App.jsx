7840c32bdba6ee55d069499ae2f3b825c120cb2d
JurayevJovohir <javokhake1@gmail.com>
2023-03-19 10:47:41 -0700

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
 import './index.scss'
 
 function App() {
+
+  useEffect();
   return (
-      <div>
-        <h2></h2>
-      </div>
-      );
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+        const newData = await response.json();
+
+        console.log(newData.title);
+      };
+      fetchData();
+    }, [])
+  );
 }
 
-      export default App;
+export default App;
